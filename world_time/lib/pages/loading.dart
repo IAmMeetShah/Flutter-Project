77c82ed805c1c8ea68d8c8b5dddade65f3cdce2f
IAmMeetShah <meetshahmsm01@gmail.com>
2022-08-17 15:58:41 +0530

    adding the world time application

---
+++ world_time/lib/pages/loading.dart
@@ -0,0 +1,45 @@
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:flutter/material.dart';
+
+import 'package:world_time/services/world_time.dart';
+
+class loading extends StatefulWidget {
+  const loading({Key? key}) : super(key: key);
+
+  @override
+  State<loading> createState() => _loadingState();
+}
+
+class _loadingState extends State<loading> {
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(
+        url: 'Europe/Berlin', location: 'Berlin', flag: 'germany.png');
+    await instance.getTime();
+
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
+      'isDayTime': instance.isDayTime
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupWorldTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green[700],
+      body: Center(
+        child: SpinKitFadingCube(
+          color: Colors.white,
+          size: 60.0,
+        ),
+      ),
+    );
+  }
+}
